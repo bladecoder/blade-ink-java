79a3bb2c967b17a2cc72ef8d73309284b1953c42
rgarcia <bladecoder@gmail.com>
2016-10-20 16:42:14 +0200

    Ref. C# commit: Starting to parse # tags, and adding them to a handful of Parsed.Object instances. It's not exhaustive, but it's a start.

---
+++ src/main/java/com/bladecoder/ink/runtime/Tag.java
@@ -0,0 +1,13 @@
+package com.bladecoder.ink.runtime;
+
+public class Tag extends RTObject {
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public Tag(String tagText) {
+		this.text = tagText;
+	}
+}
