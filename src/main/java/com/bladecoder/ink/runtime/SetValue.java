10676d284c21dcc72010a84db9cc42092e31a787
bladecoder <bladecoder@gmail.com>
2017-01-13 00:46:23 +0100

    C# ref. commit: Printing the value of a set item just prints its name. Set definitions treated like constants, so you can access them from other VAR definitions.

--- src/main/java/com/bladecoder/ink/runtime/SetValue.java
+++ src/main/java/com/bladecoder/ink/runtime/SetValue.java
@@ -95,17 +95,18 @@ public class SetValue extends Value<HashMap<String, Integer>> {
 		
 	 
 		StringBuilder sb = new StringBuilder ();
-	     sb.append ("(");
+
 	     for (int i = 0; i < ordered.size(); i++) {
 	         if (i > 0)
 	             sb.append (", ");
 	         
-	         sb.append (ordered.get(i));
-	         sb.append (": ");
-	         sb.append (value.get(ordered.get(i)));
+	         String fullItemPath = ordered.get(i);
+	         String[] nameParts = fullItemPath.split (".");
+	         String itemName = nameParts [nameParts.length - 1];
+	         
+	         sb.append (itemName);
 	     }
 	     
-	     sb.append (")");
 	 
 	     return sb.toString ();
 	 }
